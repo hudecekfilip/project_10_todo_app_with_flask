eaea2abbed2fe7899366abc3ae253cb81e6829c5
Filip Hudeček <filiphudecek@Filip-MacBook-Pro.local>
2018-10-10 13:07:04 +0200

    Some changes

--- app.py
+++ app.py
@@ -1,8 +1,15 @@
 from flask import Flask, g, jsonify, render_template
 
+from auth import auth
+from resources.users import users_api
+from resources.todos import todos_api
+
 import config
+import models
 
 app = Flask(__name__)
+app.register_blueprint(users_api)
+app.register_blueprint(todos_api)
 
 
 @app.route('/')
@@ -10,4 +17,5 @@ def my_todos():
     return render_template('index.html')
 
 if __name__ == '__main__':
+    models.initialize()
     app.run(debug=config.DEBUG, host=config.HOST, port=config.PORT)
